d1be2c1107bcc6dc2e000e67703c3fdf0c7648de
Simon Mulser <simonmulser@gmail.com>
2017-05-24 14:56:16 +0200

    move log commands in own file

---
+++ code/logs.py
@@ -0,0 +1,51 @@
+import setup
+
+
+def aggregate_logs(ids):
+    commands = []
+    timestamp_length = str(len('2016-09-22 14:46:41.706605'))
+    logfile_raw = setup.log_file + '.raw'
+
+    def prefix_lines(prefix):
+        return 'sed -e \'s/^/' + prefix + ' /\''
+
+    def remove_empty_lines():
+        return 'sed "s/^$//g"'
+
+    def remove_lines_starting_with_whitspace():
+        return 'sed "s/^[[:space:]].*$//g"'
+
+    def remove_multiline_error_messages():
+        return 'sed "s/^.\{26\}  .*$//g"'
+
+    def sed_command(_id): # insert node id after timestamp
+        return 'sed "s/^.\{' + timestamp_length + '\}/& ' + _id + '/g"'
+
+    "remove files from previous run"
+    commands.append('rm -rf ' + setup.log_file)
+    commands.append('rm -rf ' + logfile_raw)
+
+    "consolidate logfiles from the nodes"
+    commands.extend([' cat ' + setup.host(_id) + '/regtest/debug.log '
+                     ' |   ' + sed_command(_id) +
+                     ' >>  ' + logfile_raw + '; '
+                    for _id in ids])
+
+    "clean the logfiles"
+    commands.append(' cat ' + logfile_raw +
+                    ' | ' + remove_empty_lines() +
+                    ' | ' + remove_lines_starting_with_whitspace() +
+                    ' | ' + remove_multiline_error_messages() +
+                    ' > ' + setup.log_file
+                    )
+    "sort by timestamp"
+    commands.append(' sort ' + setup.log_file)
+
+    "aggregate fork information"
+    commands.extend([' cat ' + setup.host(_id) + '/chaintips.json '
+                     ' | jq "length" '
+                     ' | ' + prefix_lines(_id) +
+                     ' >> ' + setup.root_dir + '/forks; '
+                    for _id in ids])
+
+    return commands
--- code/setup.py
+++ code/setup.py
@@ -5,6 +5,7 @@ import random
 import sys
 from scheduler import Scheduler
 import dockercmd
+import logs
 
 # IP range from RFC6890 - IP range for future use
 # it does not conflict with https://github.com/bitcoin/bitcoin/blob/master/src/netbase.h
@@ -136,58 +137,8 @@ def execution_plan(nodes, number_of_blocks, block_time, latency):
 
             plan.append('docker run --rm --volume ' + root_dir + ':/mnt' + ' ' + image + ' chmod a+rwx --recursive /mnt') # fix permissions on datadirs
 
-            plan.extend(aggregate_logs(node_manager.ids))
+            plan.extend(logs.aggregate_logs(node_manager.ids))
     finally:
         plan.append('docker network rm isolated_network')
 
     return plan
-
-
-def aggregate_logs(ids):
-    commands = []
-    timestamp_length = str(len('2016-09-22 14:46:41.706605'))
-    logfile_raw = log_file + '.raw'
-
-    def prefix_lines(prefix):
-        return 'sed -e \'s/^/' + prefix + ' /\''
-
-    def remove_empty_lines():
-        return 'sed "s/^$//g"'
-
-    def remove_lines_starting_with_whitspace():
-        return 'sed "s/^[[:space:]].*$//g"'
-
-    def remove_multiline_error_messages():
-        return 'sed "s/^.\{26\}  .*$//g"'
-
-    def sed_command(_id): # insert node id after timestamp
-        return 'sed "s/^.\{' + timestamp_length + '\}/& ' + _id + '/g"'
-
-    "remove files from previous run"
-    commands.append('rm -rf ' + log_file)
-    commands.append('rm -rf ' + logfile_raw)
-
-    "consolidate logfiles from the nodes"
-    commands.extend([' cat ' + host(_id) + '/regtest/debug.log '
-                     ' |   ' + sed_command(_id) +
-                     ' >>  ' + logfile_raw + '; '
-                    for _id in ids])
-
-    "clean the logfiles"
-    commands.append(' cat ' + logfile_raw +
-                    ' | ' + remove_empty_lines() +
-                    ' | ' + remove_lines_starting_with_whitspace() +
-                    ' | ' + remove_multiline_error_messages() +
-                    ' > ' + log_file
-                    )
-    "sort by timestamp"
-    commands.append(' sort ' + log_file)
-
-    "aggregate fork information"
-    commands.extend([' cat ' + host(_id) + '/chaintips.json '
-                     ' | jq "length" '
-                     ' | ' + prefix_lines(_id) +
-                     ' >> ' + root_dir + '/forks; '
-                    for _id in ids])
-
-    return commands
