73b7748addb8aeff57f0f9a605b84034c1a7d2c2
Simon Mulser <simonmulser@gmail.com>
2017-05-25 12:50:28 +0200

    format code

--- code/dockercmd.py
+++ code/dockercmd.py
@@ -2,40 +2,33 @@ import plan
 
 
 def run_bootstrap_node(cmd):
-    return (' '
-            ' docker run '
+    return (' docker run '
             ' --detach=true '
             ' --net=isolated_network '
             ' --ip=' + plan.ip_bootstrap + ' '
             ' --name=bootstrap'   # container name
             ' ' + plan.image + ' '      # image name # src: https://hub.docker.com/r/abrkn/bitcoind/
-            '   bash -c "' + cmd + '" '
-            ' '
-            )
+            '   bash -c "' + cmd + '" ')
 
 
 def run_node(name, cmd):
-    return (' '
-            ' docker run '
-            ' --cap-add=NET_ADMIN ' # for `tc`
+    return (' docker run '
+            ' --cap-add=NET_ADMIN '  # for `tc`
             ' --detach=true '
             ' --net=isolated_network '
             ' --name=' + name + ' '   # container name
             ' --hostname=' + name + ' '
             ' --volume ' + plan.host_dir(name) + ':' + plan.guest_dir + ' '
             ' ' + plan.image + ' '      # image name # src: https://hub.docker.com/r/abrkn/bitcoind/
-            ' bash -c "' + cmd + '" '
-            ' ')
+            ' bash -c "' + cmd + '" ')
 
 
 def rm_node(name):
-    return (' '
-            ' docker rm --force ' + name)
+    return ' docker rm --force ' + name
 
 
 def exec_bash(node, command):
-    return (' '
-            ' docker exec '
+    return (' docker exec '
             + node +
             ' /bin/sh -c \''
             '    bitcoin-cli -regtest -datadir=' + plan.guest_dir + ' '  # -printtoconsole -daemon
