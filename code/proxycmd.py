3282103fc5082aec57d4ac5fe5bcda678d8f032f
Simon Mulser <simonmulser@gmail.com>
2017-06-02 16:30:35 +0200

    implement get best pulbic block hash

--- code/proxycmd.py
+++ code/proxycmd.py
@@ -1,3 +1,5 @@
+import dockercmd
+
 def run_proxy(node, start_hash):
         public_ips = [str(ip) for ip in node.public_ips]
         args = '{} '.format(node.args) if node.args else ''
@@ -5,3 +7,7 @@ def run_proxy(node, start_hash):
                 '--ip-private ' + str(node.private_ip) + ' '
                 '--ips-public ' + ' '.join(public_ips) +
                 ' --start-hash=' + start_hash)
+
+
+def get_best_public_block_hash(node):
+        return dockercmd.exec_bash(node, 'python cli.py get_best_public_block_hash')
