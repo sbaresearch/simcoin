369d72c7b6986a7f858309ee48610be3d024f606
Simon Mulser <simonmulser@gmail.com>
2017-06-05 15:33:03 +0200

    fix tests

--- code/tests/test_bitcoindcmd.py
+++ code/tests/test_bitcoindcmd.py
@@ -6,8 +6,8 @@ from plan import SelfishNode
 class TestBitcoindcmd(TestCase):
 
     def test_start_selfish_miner_private_node(self):
-        node = SelfishNode('name', '192.168.0.1', 'ip2s', 'args')
-        cmd = bitcoindcmd.start_selfish_mining(node)
+        node = SelfishNode('name', '192.168.0.1', '192.168.0.2', 'args')
+        cmd = bitcoindcmd.start_selfish_mining(node.private_node)
 
-        self.assertTrue('-addnode=192.168.0.1' in cmd)
+        self.assertTrue('-addnode=192.168.0.2' in cmd)
         self.assertTrue('  ' not in cmd)
--- code/tests/test_plan.py
+++ code/tests/test_plan.py
@@ -15,40 +15,31 @@ class TestPlan(TestCase):
         self.plan = Plan(self.config)
 
     def test_set_public_ips_with_two_nodes(self):
-        node1 = Node('1', 'ip1')
-        node2 = Node('2', 'ip2')
-        selfish_node = SelfishNode('3', 'ip3', 'ip_private', 'args')
-        self.plan.nodes = [node1, node2]
-        self.plan.selfish_nodes = [selfish_node]
+        self.config.nodes = 2
+        self.config.selfish_nodes = 1
+        self.plan = Plan(self.config)
         self.config.connectivity = 0.5
 
         self.plan.set_public_ips()
 
-        self.assertTrue(len(self.plan.selfish_nodes[0].public_ips), 1)
+        self.assertTrue(len(self.plan.selfish_nodes[0].proxy.public_ips), 1)
 
     def test_set_public_ips_with_three_nodes(self):
-        node1 = Node('1', 'ip1')
-        node2 = Node('2', 'ip2')
-        node3 = Node('3', 'ip3')
-        selfish_node = SelfishNode('4', 'ip4', 'ip_private', 'args')
-        self.plan.nodes = [node1, node2, node3]
-        self.plan.selfish_nodes = [selfish_node]
+        self.config.nodes = 3
+        self.config.selfish_nodes = 1
+        self.plan = Plan(self.config)
         self.config.connectivity = 0.5
 
         self.plan.set_public_ips()
 
-        self.assertTrue(len(self.plan.selfish_nodes[0].public_ips), 1)
+        self.assertTrue(len(self.plan.selfish_nodes[0].proxy.public_ips), 1)
 
     def test_set_public_ips_with_four_nodes(self):
-        node1 = Node('1', 'ip1')
-        node2 = Node('2', 'ip2')
-        node3 = Node('3', 'ip3')
-        selfish_node1 = SelfishNode('4', 'ip4', 'ip_private', 'args')
-        selfish_node2 = SelfishNode('5', 'ip5', 'ip_private', 'args')
-        self.plan.nodes = [node1, node2, node3]
-        self.plan.selfish_nodes = [selfish_node1, selfish_node2]
+        self.config.nodes = 3
+        self.config.selfish_nodes = 2
+        self.plan = Plan(self.config)
         self.config.connectivity = 0.5
 
         self.plan.set_public_ips()
 
-        self.assertTrue(len(self.plan.selfish_nodes[0].public_ips), 2)
+        self.assertTrue(len(self.plan.selfish_nodes[0].proxy.public_ips), 2)
