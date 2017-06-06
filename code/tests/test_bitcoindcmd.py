350ab2ca66d50be2d6c88a97c0aa6a1200861116
Simon Mulser <simonmulser@gmail.com>
2017-06-06 15:57:15 +0200

    fix test

--- code/tests/test_bitcoindcmd.py
+++ code/tests/test_bitcoindcmd.py
@@ -6,6 +6,6 @@ from plan import SelfishNode
 class TestBitcoindcmd(TestCase):
 
     def test_start_selfish_miner_private_node(self):
-        cmd = bitcoindcmd.start_selfish_mining('ip')
+        cmd = bitcoindcmd.start_selfish_mining()
 
         self.assertTrue('  ' not in cmd)
