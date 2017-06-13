204585930e4a07b041ae95ef708d4d6ce045e71e
Simon Mulser <simonmulser@gmail.com>
2017-06-13 16:00:24 +0200

    specify node which generates block or tx

--- code/tick.py
+++ code/tick.py
@@ -4,6 +4,8 @@ import random
 import pandas
 import sys
 import numpy as np
+import config
+from collections import Counter
 
 random.seed(1)
 np.random.seed(1)
@@ -30,6 +32,19 @@ def check_positive(value):
 
 parser = argparse.ArgumentParser(description='Create a simple tick.config for Bitcoin Network Simulator.')
 
+
+parser.add_argument('--nodes'
+                    , default=4
+                    , type=check_positive_int
+                    , help='Number of Bitcoin nodes which generate blocks and tx.'
+                    )
+
+parser.add_argument('--selfish-nodes'
+                    , default=1
+                    , type=check_positive_int
+                    , help='Number of selfish Bitcoin nodes which generate blocks and tx.'
+                    )
+
 parser.add_argument('--amount-of-ticks'
                     , default=60
                     , type=check_positive_int
@@ -52,6 +67,9 @@ args = parser.parse_args()
 print("arguments called with: {}".format(sys.argv))
 print("parsed arguments: {}".format(args))
 
+all_nodes = [config.node_prefix + str(i) for i in range(args.nodes)]
+all_nodes.extend([config.selfish_node_prefix + str(i) for i in range(args.selfish_nodes)])
+
 # generate 20% extra to have enough intervals
 expected_blocks = int(int(args.amount_of_ticks * (1 / args.block_interval)) * 1.2)
 expected_tx = int(int(args.amount_of_ticks * (1 / args.tx_interval)) * 1.2)
@@ -66,11 +84,20 @@ index_block = 0
 index_tx = 0
 ticks = [[] for i in range(args.amount_of_ticks)]
 for index, tick in enumerate(ticks):
+    chosen_nodes = []
     while block_point_in_time[index_block] < index + 1:
-        tick.append('block')
+        node = random.choice(all_nodes)
+        chosen_nodes.append(node)
+        tick.append('block ' + node)
         index_block += 1
+
+    most_common = Counter(chosen_nodes).most_common(1)
+    if len(most_common) > 0 and most_common[0][1] > 1:
+        raise Exception("Block interval={} is too low. "
+                        "Only one block per node per tick is allowed.".format(args.block_interval))
+
     while tx_point_in_time[index_tx] < index + 1:
-        tick.append('tx')
+        tick.append('tx ' + random.choice(all_nodes))
         index_tx += 1
 
 print(pandas.DataFrame(ticks))
