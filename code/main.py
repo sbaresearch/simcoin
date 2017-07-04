24b672abb7f1c6ecb0ad13eb81c861bb0b3c32f4
Simon Mulser <simonmulser@gmail.com>
2017-07-04 13:52:53 +0200

    avoid execution of code when main is not runned as main

--- code/main.py
+++ code/main.py
@@ -11,50 +11,50 @@ import time
 from stats import Stats
 from prepare import Prepare
 
-if sys.version_info <= (3, 0):
-    print("Sorry, requires Python 3.x or above")
-    exit()
 
-parser = argparse.ArgumentParser(description='Running Simcoin. A Bitcoin Network Simulator.')
+def main():
+    if sys.version_info <= (3, 0):
+        print("Sorry, requires Python 3.x or above")
+        exit()
 
-parser.add_argument('-v'
-                    , '--verbose'
-                    , help='Increase output verbosity'
-                    , action='store_true'
-                    )
-parser.add_argument('--selfish-nodes-args'
-                    , help='Set args for selfish nodes. '
-                           'Set them as string and if you use just one add a space at the end. Eg.: "--arg "'
-                    , default=''
-                    )
-parser.add_argument('--tick-duration'
-                    , help='Set the duration of one tick in seconds.'
-                    , default=1
-                    , type=checkargs.check_positive_float
-                    )
+    parser = argparse.ArgumentParser(description='Running Simcoin. A Bitcoin Network Simulator.')
 
-args = parser.parse_args()
+    parser.add_argument('-v'
+                        , '--verbose'
+                        , help='Increase output verbosity'
+                        , action='store_true'
+                        )
+    parser.add_argument('--selfish-nodes-args'
+                        , help='Set args for selfish nodes. '
+                               'Set them as string and if you use just one add a space at the end. Eg.: "--arg "'
+                        , default=''
+                        )
+    parser.add_argument('--tick-duration'
+                        , help='Set the duration of one tick in seconds.'
+                        , default=1
+                        , type=checkargs.check_positive_float
+                        )
 
-logFormatter = logging.Formatter("%(asctime)s.%(msecs)03d000 executor [%(threadName)-12.12s] "
-                                 "[%(levelname)-5.5s]  %(message)s", "%Y-%m-%d %H:%M:%S")
-logging.Formatter.converter = time.gmtime
-rootLogger = logging.getLogger()
+    args = parser.parse_args()
 
-fileHandler = logging.FileHandler(config.log_file, mode='w')
-fileHandler.setFormatter(logFormatter)
-rootLogger.addHandler(fileHandler)
+    logFormatter = logging.Formatter("%(asctime)s.%(msecs)03d000 executor [%(threadName)-12.12s] "
+                                     "[%(levelname)-5.5s]  %(message)s", "%Y-%m-%d %H:%M:%S")
+    logging.Formatter.converter = time.gmtime
+    rootLogger = logging.getLogger()
 
-consoleHandler = logging.StreamHandler(sys.stdout)
-consoleHandler.setFormatter(logFormatter)
-rootLogger.addHandler(consoleHandler)
+    fileHandler = logging.FileHandler(config.log_file, mode='w')
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
 
-if args.verbose:
-    rootLogger.setLevel(logging.DEBUG)
-else:
-    rootLogger.setLevel(logging.INFO)
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
 
+    if args.verbose:
+        rootLogger.setLevel(logging.DEBUG)
+    else:
+        rootLogger.setLevel(logging.INFO)
 
-def run():
     for image in [config.node_image, config.selfish_node_image]:
         if os.system("docker inspect " + image + " > /dev/null") != 0:
             raise Exception("Image {} not found".format(image))
@@ -76,4 +76,5 @@ def run():
 
     executor.execute()
 
-run()
+if __name__ == "__main__":
+    main()
