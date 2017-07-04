dc808fb96f0a8ccac2645ac8e8e2b40e5a69a4a2
Simon Mulser <simonmulser@gmail.com>
2017-07-04 16:19:50 +0200

    refactor into more methods

--- code/main.py
+++ code/main.py
@@ -13,6 +13,30 @@ from prepare import Prepare
 
 
 def main():
+    args = parse()
+
+    config_logger(args.verbose)
+
+    for image in [config.node_image, config.selfish_node_image]:
+        if os.system("docker inspect " + image + " > /dev/null") != 0:
+            raise Exception("Image {} not found".format(image))
+
+    for file in [config.network_config, config.tick_csv]:
+        if not os.path.isfile(file):
+            raise Exception("{} file not found. Please generate file before starting Simcoin.".format(file))
+
+    executor = Executor(args)
+
+    stats = Stats(executor)
+    executor.stats = stats
+
+    prepare = Prepare(executor)
+    executor.prepare = prepare
+
+    executor.execute()
+
+
+def parse():
     parser = argparse.ArgumentParser(description='Running Simcoin. A Bitcoin Network Simulator.')
 
     parser.add_argument('-v'
@@ -32,7 +56,13 @@ def main():
                         )
 
     args = parser.parse_args()
+    logging.info("arguments called with: {}".format(sys.argv))
+    logging.info("parsed arguments: {}".format(args))
 
+    return args
+
+
+def config_logger(verbose):
     log_formatter = logging.Formatter("%(asctime)s.%(msecs)03d000 executor [%(threadName)-12.12s] "
                                       "[%(levelname)-5.5s]  %(message)s", "%Y-%m-%d %H:%M:%S")
     logging.Formatter.converter = time.gmtime
@@ -46,31 +76,10 @@ def main():
     console_handler.setFormatter(log_formatter)
     root_logger.addHandler(console_handler)
 
-    if args.verbose:
+    if verbose:
         root_logger.setLevel(logging.DEBUG)
     else:
         root_logger.setLevel(logging.INFO)
-
-    for image in [config.node_image, config.selfish_node_image]:
-        if os.system("docker inspect " + image + " > /dev/null") != 0:
-            raise Exception("Image {} not found".format(image))
-
-    for file in [config.network_config, config.tick_csv]:
-        if not os.path.isfile(file):
-            raise Exception("{} file not found. Please generate file before starting Simcoin.".format(file))
-
-    logging.info("arguments called with: {}".format(sys.argv))
-    logging.info("parsed arguments: {}".format(args))
-
-    executor = Executor(args)
-
-    stats = Stats(executor)
-    executor.stats = stats
-
-    prepare = Prepare(executor)
-    executor.prepare = prepare
-
-    executor.execute()
 
 if __name__ == "__main__":
     main()
