7d63b85adec8e513ee13e14c77bad427304c0605
Simon Mulser <simonmulser@gmail.com>
2017-07-05 09:18:19 +0200

    make one general comment

--- code/tccmd.py
+++ code/tccmd.py
@@ -1,13 +1,16 @@
 import config
 
+#
+# for the 'tc' command 'iproute2' needs to be installed inside the container
+# furthermore the container needs to be started with '--cap-add=NET_ADMIN'
+#
+
 
 def add(latency):
-        # needed for this cmd: apt install iproute2 and --cap-add=NET_ADMIN
         return 'tc qdisc add dev eth0 root netem delay ' + str(latency) + 'ms'
 
 
 def add_except_ip(latency, ip):
-        # needed for this cmd: apt install iproute2 and --cap-add=NET_ADMIN
         return ['tc qdisc add dev eth0 root handle 1: prio',
                 'tc filter add dev eth0 parent 1:0 protocol ip prio 1 u32 match ip dst ' + str(ip) + ' flowid 1:1',
                 'tc filter add dev eth0 parent 1:0 protocol ip prio 1 u32 match ip dst ' + config.ip_range + ' flowid 1:2',
