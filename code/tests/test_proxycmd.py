97d5e7df11d4010efd5555e39deb84269a9d4ac6
Simon Mulser <simonmulser@gmail.com>
2017-07-05 21:15:24 +0200

    add tests for proxycmd

---
+++ code/tests/test_proxycmd.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+import proxycmd
+from node import ProxyNode
+import ipaddress
+
+
+class TestProxycmd(TestCase):
+
+    def test_run_proxy(self):
+        args = 'args1 args2'
+        node = ProxyNode('name', ipaddress.ip_address('1.1.1.1'), ipaddress.ip_address('2.2.2.1'), args)
+        cmd = proxycmd.run_proxy(node, 'hash')
+        print(cmd)
+        self.assertTrue('  ' not in cmd)
+
+    def test_get_best_public_block_hash(self):
+        cmd = proxycmd.get_best_public_block_hash('node-0')
+
+        self.assertTrue('  ' not in cmd)
+        self.assertTrue('get_best_public_block_hash' in cmd)
+
+    def test_start_hash(self):
+        cmd = proxycmd.start_hash('node-0')
+
+        self.assertTrue('  ' not in cmd)
+        self.assertTrue('start_hash' in cmd)
+
+    def test_exec_cli(self):
+        cmd = proxycmd.exec_cli('node-0', 'cmd')
+
+        self.assertTrue('  ' not in cmd)
+        self.assertTrue('python cli.py cmd' in cmd)
