78db0ed14ed991b20bc2d446204af9622125795b
Simon Mulser <simonmulser@gmail.com>
2017-07-21 13:40:14 +0200

    set txfee as small as possible

--- code/bitcoincmd.py
+++ code/bitcoincmd.py
@@ -15,7 +15,7 @@ args = {
     'listenonion':        '-listenonion=0',  # disable tor
     'onlynet':            '-onlynet=ipv4',  # disable ipv6
     'reindex':            '-reindex',
-    'paytxfee':           '-paytxfee=0.00000001',
+    'paytxfee':           '-paytxfee=0.00001',
 }
 
 
