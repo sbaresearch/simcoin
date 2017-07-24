dbf464d72ebdc8db01178cc858096b8e8ddbd593
Simon Mulser <simonmulser@gmail.com>
2017-07-24 23:41:49 +0200

    use interactive cli for intervals

--- code/intervals.py
+++ code/intervals.py
@@ -1,30 +1,34 @@
-import argparse
 import csv
 import random
 import pandas
-import sys
 import numpy as np
 import config
-import checkargs
+import utils
+import nodesconfig
 
-
-class Node:
-    def __init__(self, name):
-        self.name = name
-        self.processed = 0
-        self.block_events = []
+np.set_printoptions(precision=2, suppress=True)
 
 
 def main():
-    np.set_printoptions(precision=2, suppress=True)
+    nodes = nodesconfig.read()
+    create(False, nodes)
 
-    args = parse()
-    nodes = create_nodes(args)
 
-    random.seed(int(args.seed))
-    np.random.seed(int(args.seed))
+def create(create_all, nodes):
+    amount_of_intervals = utils.get_non_negative_int('How many intervals do you want create?\n> ')
 
-    intervals = create_intervals(nodes, args.tx_per_interval, args.amount_of_intervals)
+    intervals_per_block = utils.get_non_negative_int('How many intervals should pass by per block?\n> ')
+
+    tx_per_interval = utils.get_non_negative_int('How many transactions per interval?\n> ')
+
+    seed = utils.get_non_negative_int('Which seed do you want use?\n> ')
+
+    random.seed(seed)
+    np.random.seed(seed)
+
+    block_events = create_block_events(nodes, amount_of_intervals, intervals_per_block)
+
+    intervals = create_intervals(nodes, block_events, tx_per_interval, amount_of_intervals)
 
     print(pandas.DataFrame(intervals))
 
@@ -32,57 +36,8 @@ def main():
         writer = csv.writer(file, delimiter=';')
         writer.writerows(intervals)
 
-
-def parse():
-    parser = argparse.ArgumentParser(description='Create a simple {} for Bitcoin Network Simulator.'.format(config.interval_csv))
-
-    parser.add_argument('--nodes'
-                        , default=4
-                        , type=checkargs.check_positive_int
-                        , help='Number of Bitcoin nodes which generate blocks and tx.'
-                        )
-
-    parser.add_argument('--selfish-nodes'
-                        , default=1
-                        , type=checkargs.check_positive_int
-                        , help='Number of selfish Bitcoin nodes which generate blocks and tx.'
-                        )
-
-    parser.add_argument('--amount-of-intervals'
-                        , default=60
-                        , type=checkargs.check_positive_int
-                        , help='Amount of intervals.')
-
-    parser.add_argument('--intervals-per-block'
-                        , default=10
-                        , type=checkargs.check_positive_int
-                        , help='Intervals per block.'
-                        )
-
-    parser.add_argument('--tx-per-interval'
-                        , default=1
-                        , type=checkargs.check_positive_int
-                        , help='Tx per interval.'
-                        )
-
-    parser.add_argument('--seed'
-                        , default=0
-                        , type=checkargs.check_positive_int
-                        , help='Set the seed.'
-                        )
-
-    args = parser.parse_args()
-    print("arguments called with: {}".format(sys.argv))
-    print("parsed arguments: {}".format(args))
-
-    return args
-
-
-def create_nodes_array(number_of_nodes, number_of_selfish_nodes):
-    nodes = [config.node_name.format(str(i)) for i in range(number_of_nodes)]
-    nodes.extend([config.selfish_node_name.format(str(i)) for i in range(number_of_selfish_nodes)])
-
-    return nodes
+    if create_all:
+        pass
 
 
 def calc_expected_events(number_of_intervals, events_per_interval):
@@ -90,29 +45,21 @@ def calc_expected_events(number_of_intervals, events_per_interval):
     return int(int(number_of_intervals * (1.0 / events_per_interval)) * 3)
 
 
-def create_nodes(args):
-    nodes = [Node(node) for node in create_nodes_array(args.nodes, args.selfish_nodes)]
-    expected_blocks = calc_expected_events(args.amount_of_intervals, args.intervals_per_block)
-
-    sum_of_shares = 0
+def create_block_events(nodes, amount_of_intervals, intervals_per_block):
+    expected_blocks = calc_expected_events(amount_of_intervals, intervals_per_block)
+    block_events = {}
     for node in nodes:
-        share = float(input('Share of computation power of {} [0, 1]: '.format(node.name)))
-        node.block_events = create_block_events(share, args.intervals_per_block, expected_blocks)
-        sum_of_shares += share
-    sum_of_shares = round(sum_of_shares, 2)
-    if sum_of_shares != 1:
-        raise Exception('Sum of shares should be 1. It was {} instead.'.format(sum_of_shares))
-
-    return nodes
+        block_events[node.name] = create_block_series(node.share, intervals_per_block, expected_blocks)
+    return block_events
 
 
-def create_block_events(share, intervals_per_block, expected_blocks):
+def create_block_series(share, intervals_per_block, expected_blocks):
     random_event_intervals = np.random.exponential(intervals_per_block * (1 / share), expected_blocks)
     block_events = np.cumsum(random_event_intervals)
     return block_events
 
 
-def create_intervals(nodes, tx_per_interval, amount_of_intervals):
+def create_intervals(nodes, block_events, tx_per_interval, amount_of_intervals):
     index_tx = 0
     intervals = [[] for _ in range(amount_of_intervals)]
     for index, interval in enumerate(intervals):
@@ -120,12 +67,13 @@ def create_intervals(nodes, tx_per_interval, amount_of_intervals):
             interval.append('tx ' + random.choice(nodes).name)
             index_tx += 1
 
-        for node in nodes:
-            processed = node.processed
-            while node.block_events[node.processed] < index + 1:
-                interval.append('block ' + node.name)
-                node.processed += 1
-            if node.processed - processed > 1:
+        for node in block_events.keys():
+            pop_count = 0
+            while block_events[node][0] < index + 1:
+                interval.append('block ' + node)
+                block_events[node].pop(0)
+                pop_count += 1
+            if pop_count > 1:
                 raise Exception("Intervals per block is too low. Only one block per node per interval is allowed. "
                                 "Raise the intervals_per_block or try a different seeed. ")
 
