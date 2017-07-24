6fd083acea49d4e45b544656211b0cd8754a4d97
Simon Mulser <simonmulser@gmail.com>
2017-07-24 23:47:57 +0200

    use objects instead of dicts in test class

--- code/tests/test_nodesconfig.py
+++ code/tests/test_nodesconfig.py
@@ -3,6 +3,7 @@ from mock import patch
 from mock import mock_open
 import nodesconfig
 import config
+from node.bitcoinnode import BitcoinNodeConfig
 
 
 class TestNodesConfig(TestCase):
@@ -13,15 +14,15 @@ class TestNodesConfig(TestCase):
     @patch('json.dumps')
     @patch('nodesconfig.check_if_share_sum_is_1')
     def test_create(self, m_check_if_share_sum_is_1, m_dumps, m_get_boolean, m_get_node_type, m_open):
-        m_get_node_type.return_value = 'normal'
-        nodesconfig.node_types = {'normal': lambda: [{'name': 'node-1'}, {'name': 'node-2'}]}
+        m_get_node_type.return_value = 'bitcoin'
+        nodesconfig.node_types = {'bitcoin': lambda: [BitcoinNodeConfig(name='node-1'), BitcoinNodeConfig(name='node-2')]}
         m_get_boolean.return_value = False
         m_dumps.return_value = 'mock'
         m_check_if_share_sum_is_1.return_value = True
 
         nodesconfig.create(False)
 
-        self.assertEqual(m_dumps.call_args[0][0],  [{'name': 'node-1'}, {'name': 'node-2'}])
+        self.assertEqual(len(m_dumps.call_args[0][0]),  2)
         m_open.assert_called_with(config.nodes_config_json, 'w')
         handle = m_open()
         self.assertEqual(handle.write.call_args[0][0], 'mock')
@@ -33,14 +34,14 @@ class TestNodesConfig(TestCase):
     @patch('nodesconfig.check_if_share_sum_is_1')
     def test_create_two_node_types(self, m_check_if_share_sum_is_1, m_dumps, m_get_boolean, m_get_node_type, m_open):
         m_get_node_type.return_value = 'normal'
-        nodesconfig.node_types = {'normal': lambda: [{'name': 'node-1'}]}
+        nodesconfig.node_types = {'normal': lambda: [BitcoinNodeConfig(name='node-1')]}
         m_get_boolean.side_effect = [True, False]
         m_dumps.return_value = 'mock'
         m_check_if_share_sum_is_1.return_value = True
 
         nodesconfig.create(False)
 
-        self.assertEqual(m_dumps.call_args[0][0],  [{'name': 'node-1'}, {'name': 'node-1'}])
+        self.assertEqual(len(m_dumps.call_args[0][0]), 2)
 
     @patch('builtins.open', new_callable=mock_open)
     @patch('utils.get_node_type')
@@ -50,7 +51,7 @@ class TestNodesConfig(TestCase):
     def test_create_computation_sum_not_1(self, m_dumps, m_check_if_share_sum_is_1, m_get_boolean, m_get_node_type, m_open):
         m_get_node_type.return_value = 'normal'
         m_check_if_share_sum_is_1.side_effect = [False, True]
-        nodesconfig.node_types = {'normal': lambda:  [{'name': 'node-1', 'share': 1}]}
+        nodesconfig.node_types = {'normal': lambda:  [BitcoinNodeConfig(name='node-1', share=0.4)]}
         m_get_boolean.return_value = False
         m_dumps.return_value = 'mock'
 
@@ -58,23 +59,12 @@ class TestNodesConfig(TestCase):
 
         self.assertEqual(m_check_if_share_sum_is_1.call_count, 2)
 
-    @patch('utils.get_non_negative_int')
-    @patch('utils.get_percentage')
-    def test_create_normal_node_config(self, m_get_percentage, m_get_non_negative_int):
-        m_get_percentage.return_value = 0.1
-        m_get_non_negative_int.side_effect = [2, 3, 4]
-
-        nodes = nodesconfig.create_normal_node_config()
-
-        self.assertEqual(nodes, [{'type': 'normal', 'latency': 3, 'share': 0.1},
-                                 {'type': 'normal', 'latency': 4, 'share': 0.1}])
-
     def test_check_if_share_sum_is_1_false(self):
-        nodes = [{'share': 0.4}, {'share': 0.4}]
+        nodes = [BitcoinNodeConfig(share=0.4), BitcoinNodeConfig(share=0.4)]
 
         self.assertFalse(nodesconfig.check_if_share_sum_is_1(nodes))
 
     def test_check_if_share_sum_is_1_true(self):
-        nodes = [{'share': 0.4}, {'share': 0.6}]
+        nodes = [BitcoinNodeConfig(share=0.4), BitcoinNodeConfig(share=0.6)]
 
         self.assertTrue(nodesconfig.check_if_share_sum_is_1(nodes))
