11d1f2e112ae946933e97e8ed8fa1c02865c511a
Simon Mulser <simonmulser@gmail.com>
2017-07-25 09:15:45 +0200

    use cli menu for connectivity

--- code/networktopology.py
+++ code/networktopology.py
@@ -1,24 +1,27 @@
-import argparse
 import csv
 import random
 import pandas
-import sys
 import config
-import checkargs
-import logging
-import intervals
+import nodesconfig
+import utils
 
 
 def main():
-    random.seed(1)
+    nodes = nodesconfig.read()
+    create(False, nodes)
 
-    args = parse()
 
-    nodes = args.nodes
-    selfish_nodes = args.selfish_nodes
+def create(create_all, nodes):
 
-    header = create_header(nodes, selfish_nodes)
-    matrix = create_matrix(header, args.latency, args.connectivity)
+    seed = utils.get_non_negative_int('Which seed do you want use?\n> ')
+
+    connectivity = utils.get_percentage('What should be the connectivity?\n> ')
+
+    random.seed(seed)
+
+    header = create_header(nodes)
+
+    matrix = create_matrix(header, connectivity)
 
     if check_if_fully_connected(matrix) is not True:
         raise Exception("Not all nodes a reachable. Consider to raise the connectivity.")
@@ -29,49 +32,18 @@ def main():
         writer = csv.writer(file, delimiter=';')
         writer.writerows(matrix)
 
-
-def parse():
-    parser = argparse.ArgumentParser(description='Create a simple network.config for Bitcoin Network Simulator.')
-
-    parser.add_argument('--nodes'
-                        , default=4
-                        , type=checkargs.check_positive_int
-                        , help='Number of Bitcoin nodes.'
-                        )
-
-    parser.add_argument('--selfish-nodes'
-                        , default=1
-                        , type=checkargs.check_positive_int
-                        , help='Number of selfish Bitcoin nodes.'
-                        )
-
-    parser.add_argument('--latency'
-                        , default=200
-                        , type=checkargs.check_positive_int
-                        , help='Latency of nodes in milliseconds.'
-                        )
-
-    parser.add_argument('--connectivity'
-                        , default=.5
-                        , type=checkargs.check_percentage
-                        , help='Connectivity between nodes.'
-                        )
-
-    args = parser.parse_args()
-    print("arguments called with: {}".format(sys.argv))
-    print("parsed arguments: {}".format(args))
-
-    return args
+    if create_all:
+        pass
 
 
-def create_header(nodes, selfish_nodes):
+def create_header(nodes):
     header = ['']
-    header.extend(intervals.create_nodes_array(nodes, selfish_nodes))
+    header.extend([node.name for node in nodes])
 
     return header
 
 
-def create_matrix(header, latency, connectivity):
+def create_matrix(header, connectivity):
     length = len(header)
     matrix = [[] for _ in range(length)]
 
@@ -83,7 +55,7 @@ def create_matrix(header, latency, connectivity):
     for i in range(1, length):
         for j in range(1, i + 1):
             if i is j:
-                matrix[i][i] = latency
+                matrix[i][i] = 0
             elif random.random() < connectivity:
                 matrix[i][j] = matrix[j][i] = 1
             else:
@@ -111,21 +83,6 @@ def recursive_check(matrix, visited=None, start=1):
     return output
 
 
-def read_amount_of_nodes():
-    nodes = selfish_nodes = 0
-    network_config = pandas.read_csv(open(config.network_config), delimiter=';', index_col=0)
-
-    for node_row, row in network_config.iterrows():
-        if node_row.startswith(config.node_prefix):
-            nodes += 1
-        elif node_row.startswith(config.selfish_node_prefix):
-            selfish_nodes += 1
-        else:
-            raise Exception('Unknown node type in {}'.format(config.network_config))
-    logging.info('Parsed {} nodes and {} selfish nodes from {}'.format(nodes, selfish_nodes, config.network_config))
-    return nodes, selfish_nodes
-
-
 def read_connections():
     connections = {}
     network_config = pandas.read_csv(open(config.network_config), delimiter=';', index_col=0)
@@ -144,21 +101,6 @@ def read_connections():
 
     return connections
 
-
-def read_latencies():
-    latencies = {}
-    network_config = pandas.read_csv(open(config.network_config), delimiter=';', index_col=0)
-
-    for node_row, row in network_config.iterrows():
-        if node_row.startswith(config.selfish_node_prefix):
-            node_row += config.selfish_node_proxy_postfix
-        for node_column, value in row.iteritems():
-            if node_column.startswith(config.selfish_node_prefix):
-                node_column += config.selfish_node_proxy_postfix
-            if node_column == node_row:
-                latencies[node_column] = value
-
-    return latencies
 
 if __name__ == "__main__":
     main()
--- code/tests/test_networktopology.py
+++ code/tests/test_networktopology.py
@@ -3,25 +3,22 @@ import networktopology
 from mock import patch
 from mock import mock_open
 from textwrap import dedent
+from node.bitcoinnode import BitcoinNodeConfig
 
 
 class TestNetworktopology(TestCase):
 
-    @patch('intervals.create_nodes_array')
-    def test_create_header(self, mock):
-        mock.return_value = ['node-0', 'selfish-node-1']
-
-        header = networktopology.create_header(0, 0)
+    def test_create_header(self, ):
+        header = networktopology.create_header([BitcoinNodeConfig(name='node-1'), BitcoinNodeConfig(name='node-2')])
 
         self.assertEqual(len(header), 3)
-        self.assertEqual(header[0], '')
+        self.assertEqual(header, ['', 'node-1', 'node-2'])
 
     def test_create_matrix_full_connection(self):
         header = ['', 'node-0', 'node-1', 'node-2']
-        latency = 200
         connectivity = 1
 
-        matrix = networktopology.create_matrix(header, latency, connectivity)
+        matrix = networktopology.create_matrix(header, connectivity)
         for i in range(1, len(header)):
             for j in range(1, len(header)):
                 if i != j:
@@ -29,25 +26,13 @@ class TestNetworktopology(TestCase):
 
     def test_create_matrix_no_connection(self):
         header = ['', 'node-0', 'node-1', 'node-2']
-        latency = 200
         connectivity = 0
 
-        matrix = networktopology.create_matrix(header, latency, connectivity)
+        matrix = networktopology.create_matrix(header, connectivity)
         for i in range(1, len(matrix)):
             for j in range(1, len(matrix)):
                 if i != j:
                     self.assertEqual(matrix[i][j], 0)
-
-    def test_create_matrix_latency(self):
-        header = ['', 'node-0', 'node-1', 'node-2']
-        latency = 200
-        connectivity = 1
-
-        matrix = networktopology.create_matrix(header, latency, connectivity)
-        for i in range(1, len(header)):
-            for j in range(1, len(header)):
-                if i == j:
-                    self.assertEqual(matrix[i][j], latency)
 
     DATA_1 = dedent("""
         ;node-0;node-1;selfish-node-0
@@ -55,25 +40,6 @@ class TestNetworktopology(TestCase):
         node-1;1;2;1
         selfish-node-0;0;1;3
         """).strip()
-
-    @patch("builtins.open", mock_open(read_data=DATA_1))
-    def test_read_amount_of_nodes(self):
-        nodes, selfish_nodes = networktopology.read_amount_of_nodes()
-
-        self.assertEqual(nodes, 2)
-        self.assertEqual(selfish_nodes, 1)
-
-    DATA_2 = dedent("""
-        ;some-node-0
-        some-node-0;0
-        """).strip()
-
-    @patch("builtins.open", mock_open(read_data=DATA_2))
-    def test_read_amount_of_nodes_unknown_node(self):
-        with self.assertRaises(Exception) as context:
-            networktopology.read_amount_of_nodes()
-
-        self.assertTrue('Unknown node type in network.csv' in str(context.exception))
 
     @patch("builtins.open", mock_open(read_data=DATA_1))
     def test_read_connections(self):
@@ -84,16 +50,6 @@ class TestNetworktopology(TestCase):
         self.assertEqual(connections['node-0'], ['node-1'])
         self.assertEqual(connections['node-1'], ['node-0', 'selfish-node-0-proxy'])
         self.assertEqual(connections['selfish-node-0-proxy'], ['node-1'])
-
-    @patch("builtins.open", mock_open(read_data=DATA_1))
-    def test_read_latencies(self):
-        latencies = networktopology.read_latencies()
-
-        self.assertEqual(len(latencies.keys()), 3)
-
-        self.assertEqual(latencies['node-0'], 1)
-        self.assertEqual(latencies['node-1'], 2)
-        self.assertEqual(latencies['selfish-node-0-proxy'], 3)
 
     def test_check_if_fully_connected_1(self):
         matrix = [
