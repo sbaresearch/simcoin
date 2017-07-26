c6e8b676498daffddaa45d0882a77dbb2857ebb0
Simon Mulser <simonmulser@gmail.com>
2017-07-26 10:18:01 +0200

    use argparse for simulate cmd

--- code/simulation.py
+++ code/simulation.py
@@ -6,25 +6,47 @@ import time
 from stats import Stats
 from event import Event
 import utils
+import argparse
+import checkargs
+import sys
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--interval-duration'
+                        , default=1
+                        , type=checkargs.check_positive_int
+                        , help='Duration of intervals.')
+
+    parser.add_argument('--verbose'
+                        , default=False
+                        , type=bool
+                        , help='Verbose log.'
+                        )
+
+    args = parser.parse_args(sys.argv[2:])
+    print("arguments called with: {}".format(sys.argv))
+    print("parsed arguments: {}".format(args))
+    return args
 
 
 def run():
+
+    args = parse()
 
     for file in [config.network_config, config.interval_csv, config.nodes_config_json]:
         if not os.path.isfile(file):
             raise Exception("{} file not found. Please generate file before starting Simcoin.".format(file))
 
-    interval_duration = utils.get_non_negative_int('How long should one interval last? [s]\n> ')
-
-    verbose = utils.get_boolean('Should the logging be verbose? [yes/no]\n> ')
-    utils.config_logger(verbose)
+    utils.config_logger(args.verbose)
 
     executor = Executor()
 
     stats = Stats(executor)
     executor.stats = stats
 
-    event = Event(executor, interval_duration)
+    event = Event(executor, args.interval_duration)
     executor.event = event
 
     start = time.time()
