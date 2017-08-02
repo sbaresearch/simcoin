a9cd2df96bf91b1f6354b547622f6e80493b80b9
Simon Mulser <simonmulser@gmail.com>
2017-08-02 10:37:16 +0200

    set prio 2 for default filter

--- code/tccmd.py
+++ code/tccmd.py
@@ -15,7 +15,7 @@ def add_except_ip(node, latency, ip):
         cmds = [dockercmd.exec_cmd(node, cmd) for cmd in
                 ['tc qdisc add dev eth0 root handle 1: prio',
                  'tc filter add dev eth0 parent 1:0 protocol ip prio 1 u32 match ip dst {} flowid 1:1'.format(str(ip)),
-                 'tc filter add dev eth0 parent 1:0 protocol ip prio 1 u32 match ip dst {} flowid 1:2'.format(config.ip_range),
+                 'tc filter add dev eth0 parent 1:0 protocol ip prio 2 u32 match ip dst {} flowid 1:2'.format(config.ip_range),
                  'tc qdisc add dev eth0 parent 1:1 handle 10: netem delay 0ms',
                  'tc qdisc add dev eth0 parent 1:2 handle 20: netem delay {}ms'.format(str(latency))]]
         return cmds
