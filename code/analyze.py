39fb721b6c3cea64f441181a3c61289382df3a0c
Simon Mulser <simonmulser@gmail.com>
2017-08-17 17:07:05 +0200

    use csv module

--- code/analyze.py
+++ code/analyze.py
@@ -1,6 +1,7 @@
 import config
 import logging
 from utils import Stats
+import csv
 
 
 class Analyzer:
@@ -17,8 +18,9 @@ class Analyzer:
 
     def create_block_csv(self):
         with open(config.blocks_csv, 'w') as file:
-            file.write('block_hash;node;timestamp;stale;height;total_size;txs;'
-                       'total_received;median_propagation;std_propagation\n')
+            w = csv.writer(file, delimiter=';')
+            w.writerow(['block_hash', 'node', 'timestamp', 'stale', 'height', 'total_size', 'txs',
+                       'total_received', 'median_propagation', 'std_propagation'])
             for block in self.context.parsed_blocks.values():
 
                 propagation_stats = Stats.from_array(block.receiving_timestamps)
@@ -27,31 +29,32 @@ class Analyzer:
                 if block.block_hash in self.context.consensus_chain:
                     stale = False
 
-                file.write('{};{};{};{};{};{};{};{};{};{}\n'.format(
-                    block.block_hash, block.node, block.timestamp, stale, block.height, block.total_size, block.txs,
-                    propagation_stats.count, propagation_stats.median, propagation_stats.std))
+                w.writerow([block.block_hash, block.node, block.timestamp, stale, block.height, block.total_size,
+                                 block.txs, propagation_stats.count, propagation_stats.median, propagation_stats.std])
 
     def create_tx_csv(self):
         with open(config.txs_csv, 'w') as file:
-            file.write('tx_hash;node;timestamp;total_accepted;median_propagation;std_propagation\n')
+            w = csv.writer(file, delimiter=';')
+            w.writerow(['tx_hash', 'node', 'timestamp', 'total_accepted', 'median_propagation', 'std_propagation'])
 
             for tx in self.context.parsed_txs.values():
                 propagation_stats = Stats.from_array(tx.receiving_timestamps)
 
-                file.write('{};{};{};{};{};{}\n'.format(
-                    tx.tx_hash, tx.node, tx.timestamp,
-                    propagation_stats.count, propagation_stats.median, propagation_stats.std))
+                w.writerow([tx.tx_hash, tx.node, tx.timestamp,
+                                propagation_stats.count, propagation_stats.median, propagation_stats.std])
 
     def create_tx_exceptions_csv(self):
         with open(config.tx_exceptions_csv, 'w') as file:
-            file.write('node;timestamp;error_message\n')
+            w = csv.writer(file, delimiter=';')
+            w.writerow(['node', 'timestamp', 'error_message'])
 
             for exce in self.context.tx_exceptions:
-                file.write('{};{};{}\n'.format(exce.node, exce.timestamp, exce.error_message))
+                w.writerow([exce.node, exce.timestamp, exce.error_message])
 
     def create_mempool_snapshots_csv(self):
         with open(config.mempool_snapshots_csv, 'w') as file:
-            file.write('timestamp;node;txs;inputs\n')
+            w = csv.writer(file, delimiter=';')
+            w.writerow(['timestamp', 'node', 'txs', 'inputs'])
 
             for snapshot in self.context.mempool_snapshots:
-                file.write('{};{};{};{}\n'.format(snapshot.timestamp, snapshot.node, snapshot.txs, snapshot.inputs))
+                w.writerow([snapshot.timestamp, snapshot.node, snapshot.txs, snapshot.inputs])
