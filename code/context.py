3a79e2816ffd5192a53e39c86f5c5a2dbca6e0e2
Simon Mulser <simonmulser@gmail.com>
2017-08-23 16:45:49 +0200

    remove unused imports

--- code/context.py
+++ code/context.py
@@ -1,11 +1,7 @@
-import dockercmd
 import config
 from node import PublicBitcoinNode
 from node import SelfishPrivateNode
 from node import ProxyNode
-import logging
-import bash
-import prepare
 import utils
 from simulationfiles import network_config
 from simulationfiles import nodes_config
