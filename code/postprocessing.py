2fa8a27cffd8f950fed78e4293384f948863e7b9
Simon Mulser <simonmulser@gmail.com>
2017-08-26 16:14:50 +0200

    add lines to normal log

--- code/postprocessing.py
+++ code/postprocessing.py
@@ -31,29 +31,42 @@ class PostProcessing:
 
     def aggregate_logs(self):
         for node in self.context.all_nodes.values():
-            content = node.cat_log_cmd().splitlines()
-            content = prefix_log(content, node.name)
+            lines = node.cat_log_cmd().splitlines()
+            lines = prefix_log(lines, node.name)
+            lines = add_line_number(lines)
 
             with open(config.aggregated_log, 'a') as file:
-                file.write('\n'.join(content) + '\n')
+                file.write('\n'.join(lines) + '\n')
 
-        bash.check_output('cat {} >> {}'.format(config.log_file, config.aggregated_log))
+        lines = bash.check_output_without_log('cat {}'.format(config.log_file)).splitlines()
+        lines = add_line_number(lines)
+        with open(config.aggregated_log, 'a') as file:
+            file.write('\n'.join(lines) + '\n')
+
         bash.check_output('sort {} -o {}'.format(config.aggregated_log, config.aggregated_log))
 
 
 def prefix_log(lines, node_name):
     prev_match = ''
     prefixed_lines = []
-    line_number = 1
     for line in lines:
         match = re.match(config.log_prefix_timestamp, line)
         if match:
             prefixed_lines.append(re.sub(config.log_prefix_timestamp
-                                         , r'\1 {} {}'.format(node_name, line_number)
+                                         , r'\1 {}'.format(node_name)
                                          , line))
             prev_match = match.group(0)
         else:
-            prefixed_lines.append('{} {} {} {}'.format(prev_match, node_name, line_number, line))
+            prefixed_lines.append('{} {} {}'.format(prev_match, node_name, line))
+    return prefixed_lines
+
+
+def add_line_number(lines):
+    prefixed_lines = []
+    line_number = 0
+    for line in lines:
+        prefixed_lines.append(re.sub(config.log_prefix_timestamp + ' ([a-zA-Z0-9-\.]+) (.*)',
+                                     r'\1 \2 {} \3'.format(line_number), line))
         line_number += 1
     return prefixed_lines
 
--- code/tests/test_postprocessing.py
+++ code/tests/test_postprocessing.py
@@ -6,6 +6,7 @@ from mock import MagicMock
 import config
 import postprocessing
 from textwrap import dedent
+from collections import OrderedDict
 
 
 class TestPostProcessing(TestCase):
@@ -14,52 +15,50 @@ class TestPostProcessing(TestCase):
         self.postprocessing = PostProcessing(self.context)
 
     @patch('builtins.open', new_callable=mock_open)
-    @patch('bash.check_output_without_log')
-    @patch('postprocessing.prefix_log')
-    def test_aggregate_logs(self, m_prefix, m_bash, m_open):
+    @patch('bash.check_output_without_log', lambda cmd: '3\n4\n')
+    @patch('postprocessing.prefix_log', lambda lines, node: lines)
+    @patch('postprocessing.add_line_number', lambda lines: lines)
+    def test_aggregate_logs(self, m_open):
         node_0 = MagicMock()
+        node_0.cat_log_cmd.return_value = "1\n2\n"
         self.context.all_nodes = {'node-0': node_0}
-        m_prefix.return_value = ['1', '2']
 
         self.postprocessing.aggregate_logs()
 
         self.assertEqual(m_open.call_args[0][0], config.aggregated_log)
         handle = m_open()
-        self.assertEqual(handle.write.call_args[0][0], '1\n2\n')
-
-        self.assertEqual(m_bash.call_count, 2)
+        self.assertEqual(handle.write.call_args_list[0][0][0], '1\n2\n')
+        self.assertEqual(handle.write.call_args_list[1][0][0], '3\n4\n')
 
     @patch('builtins.open', new_callable=mock_open)
-    @patch('bash.check_output_without_log')
-    @patch('postprocessing.prefix_log')
-    def test_aggregate_logs_two_nodes(self, m_prefix, m_bash, m_open):
+    @patch('bash.check_output_without_log', lambda cmd: '')
+    @patch('postprocessing.prefix_log', lambda lines, node: lines)
+    @patch('postprocessing.add_line_number', lambda lines: lines)
+    def test_aggregate_logs_two_nodes(self, m_open):
         node_0 = MagicMock()
         node_1 = MagicMock()
-        m_prefix.side_effect = [['1', '2'], ['11', '22']]
-        self.context.all_nodes = {'node-0': node_0, 'node-1': node_1}
+        node_0.cat_log_cmd.return_value = "1\n2\n"
+        node_1.cat_log_cmd.return_value = "11\n22\n"
+        self.context.all_nodes = OrderedDict(sorted({'node-0': node_0, 'node-1': node_1}.items(), key=lambda t: t[0]))
 
         self.postprocessing.aggregate_logs()
 
         handle = m_open()
-        contents = [line[0][0] for line in handle.write.call_args_list]
-        self.assertEqual(len(contents), 2)
-        self.assertTrue('1\n2\n' in contents)
-        self.assertTrue('11\n22\n' in contents)
-
-        self.assertEqual(m_bash.call_count, 2)
+        self.assertEqual(handle.write.call_args_list[0][0][0], '1\n2\n')
+        self.assertEqual(handle.write.call_args_list[1][0][0], '11\n22\n')
 
     def test_prefix_log_no_changes(self):
         lines = ['2017-07-05 14:33:35.324000 test', '2017-07-05 14:33:35.324000 test']
         received = postprocessing.prefix_log(lines, 'node-0')
 
-        expected = ['2017-07-05 14:33:35.324000 node-0 1 test', '2017-07-05 14:33:35.324000 node-0 2 test']
+        expected = ['2017-07-05 14:33:35.324000 node-0 test', '2017-07-05 14:33:35.324000 node-0 test']
         self.assertEqual(received, expected)
 
     def test_prefix_log(self):
         lines = ['2017-07-05 14:33:35.324000 test', 'test']
         received = postprocessing.prefix_log(lines, 'node-0')
 
-        expected = ['2017-07-05 14:33:35.324000 node-0 1 test', '2017-07-05 14:33:35.324000 node-0 2 test']
+        expected = ['2017-07-05 14:33:35.324000 node-0 test', '2017-07-05 14:33:35.324000 node-0 test']
         self.assertEqual(received, expected)
 
     def test_cut_log(self):
