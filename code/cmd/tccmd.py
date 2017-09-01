f8cd409338be71863662b9624cf3d69df7f57215
Andreas Kern <andreas.kern@gmail.com>
2017-09-01 15:49:44 +0200

    [style] fix overlong line (PEP8)

--- code/cmd/tccmd.py
+++ code/cmd/tccmd.py
@@ -11,15 +11,22 @@ def create(node, zones, latency):
     sorted_zones = sorted(zones.items(), key=operator.itemgetter(0))
 
     cmds = [
-        'tc qdisc add dev eth0 root handle 1: prio bands {} priomap 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
-            .format(len(zones) + 1)
+        'tc qdisc add dev eth0 '
+        'root handle 1: prio bands '
+        '{}'
+        'priomap 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
+        .format(len(zones) + 1)
     ]
 
     for index, zone_tuple in enumerate(sorted_zones):
         zone = zone_tuple[1]
 
-        cmds.append('tc filter add dev eth0 parent 1: protocol ip prio {} u32'
-                    ' match ip dst {} flowid 1:{}'.format(index + 1, zone.network, index + 2))
+        cmds.append(
+            'tc filter add dev eth0 '
+            'parent 1: protocol ip prio {} u32'
+            ' match ip dst {} flowid 1:{}'
+            .format(index + 1, zone.network, index + 2)
+        )
 
     cmds.append('tc qdisc add dev eth0 parent 1:1 handle 10: netem delay 0ms')
 
@@ -30,9 +37,12 @@ def create(node, zones, latency):
             aggregated_latency = latency
         else:
             aggregated_latency = latency + zone.latency
-        cmds.append('tc qdisc add dev eth0 parent 1:{} handle {}: netem delay {}ms'.format(
-            index + 2, (index + 2) * 10, aggregated_latency
-        ))
+
+        cmds.append(
+            'tc qdisc add dev eth0 '
+            'parent 1:{} handle {}: netem delay {}ms'
+            .format(index + 2, (index + 2) * 10, aggregated_latency)
+        )
 
     docker_cmds = [dockercmd.exec_cmd(node, cmd) for cmd in cmds]
 
