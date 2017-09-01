98a4f91261ba19997062f776cd85ed3f07594c12
Andreas Kern <andreas.kern@gmail.com>
2017-09-01 16:11:57 +0200

    [style] fix overlong line (PEP8)

--- code/node.py
+++ code/node.py
@@ -12,8 +12,10 @@ from collections import OrderedDict
 from collections import namedtuple
 from bitcoin.wallet import CBitcoinSecret
 from bitcoinrpc.authproxy import JSONRPCException
-from bitcoin.core import lx, b2x, COutPoint, CMutableTxOut, CMutableTxIn, CMutableTransaction, Hash160
-from bitcoin.core.script import CScript, OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_CHECKSIG, SignatureHash, SIGHASH_ALL
+from bitcoin.core import lx, b2x, COutPoint, CMutableTxOut, CMutableTxIn, \
+    CMutableTransaction, Hash160
+from bitcoin.core.script import CScript, OP_DUP, OP_HASH160, OP_EQUALVERIFY,\
+    OP_CHECKSIG, SignatureHash, SIGHASH_ALL
 from bitcoin.wallet import CBitcoinAddress
 from http.client import CannotSendRequest
 from bitcoinrpc.authproxy import HTTP_TIMEOUT
@@ -47,13 +49,16 @@ class BitcoinNode(Node):
 
     def run(self):
         bash.check_output(bitcoincmd.start(self))
-        # sleep small amount to avoid 'CannotSendRequest: Request-sent' in bitcoinrpc
+        # sleep small amount to avoid
+        # 'CannotSendRequest: Request-sent'
+        # in bitcoinrpc
         utils.sleep(0.2)
 
     def rm(self):
         if bash.check_output(dockercmd.check_if_running(self.name)) == 'true':
             self.execute_rpc('stop')
-            while bash.check_output(dockercmd.check_if_running(self.name)) == 'true':
+            while bash.check_output(dockercmd.check_if_running(self.name)) \
+                    == 'true':
                 utils.sleep(0.4)
                 logging.debug('Wait until container stops')
         super(BitcoinNode, self).rm()
@@ -64,12 +69,16 @@ class BitcoinNode(Node):
                 self.execute_rpc('getnetworkinfo')
                 break
             except JSONRPCException as exce:
-                logging.debug('Exception="{}" while calling RPC. Waiting until RPC of node={} is ready.'
-                              .format(exce, self.name))
+                logging.debug(
+                    'Exception="{}" while calling RPC. '
+                    'Waiting until RPC of node={} is ready.'
+                    .format(exce, self.name)
+                )
                 utils.sleep(0.2)
 
     def connect_to_rpc(self, timeout=HTTP_TIMEOUT):
-        self.rpc_connection = AuthServiceProxy(config.create_rpc_connection_string(self.ip), timeout=timeout)
+        self.rpc_connection = AuthServiceProxy(
+            config.create_rpc_connection_string(self.ip), timeout=timeout)
 
     def delete_peers_file(self):
         return bash.check_output(bitcoincmd.rm_peers(self.name))
@@ -85,69 +94,116 @@ class BitcoinNode(Node):
                 if error.errno == errno.EPIPE:
                     retry -= 1
                     self.connect_to_rpc()
-                    logging.warning('Node={} could not execute RPC-call={} because of error="{}".'
-                                    ' Reconnecting RPC and retrying.'.format(self.name, args[0], error))
+                    logging.warning(
+                        'Node={} could not execute RPC-call={} '
+                        'because of error="{}". '
+                        ' Reconnecting RPC and retrying.'
+                        .format(self.name, args[0], error)
+                    )
             except CannotSendRequest as exce:
                 retry -= 1
                 self.connect_to_rpc()
-                logging.warning('Node={} could not execute RPC-call={} because of error="{}".'
-                                ' Reconnecting RPC and retrying.'.format(self.name, args[0], exce))
+                logging.warning(
+                    'Node={} could not execute RPC-call={} '
+                    'because of error="{}".'
+                    ' Reconnecting RPC and retrying.'
+                    .format(self.name, args[0], exce)
+                )
 
-        logging.error('Could not execute RPC-call={} on node {}'.format(args[0], self.name))
+        logging.error(
+            'Could not execute RPC-call={} on node {}'
+            .format(args[0], self.name)
+        )
         exit(-1)
 
     def grep_log_for_errors(self):
-        return bash.check_output_without_log(config.log_error_grep.format(config.client_dir_on_host(self) + BitcoinNode.log_file))
+        return bash.check_output_without_log(
+            config.log_error_grep.format(
+                config.client_dir_on_host(self) + BitcoinNode.log_file
+            )
+        )
 
     def cat_log_cmd(self):
-        return bash.check_output_without_log('cat ' + config.client_dir_on_host(self) + BitcoinNode.log_file)
+        return bash.check_output_without_log(
+            'cat ' + config.client_dir_on_host(self) + BitcoinNode.log_file
+        )
 
     def transfer_coinbases_to_normal_tx(self):
         for tx_chain in self.tx_chains:
             tx_chain.amount /= 2
             tx_chain.amount -= int(config.transaction_fee / 2)
-            raw_transaction = self.execute_rpc('createrawtransaction',
-                                               [{
-                                                   'txid': tx_chain.current_unspent_tx,
-                                                   'vout': 0,
-                                               }],
-                                               OrderedDict(
-                                                   [
-                                                       (tx_chain.address, tx_chain.amount / 100000000),
-                                                       (self.spent_to.address, tx_chain.amount / 100000000)
-                                                   ])
-                                               )
-            signed_raw_transaction = self.execute_rpc('signrawtransaction', raw_transaction)['hex']
-            tx_chain.current_unspent_tx = self.execute_rpc('sendrawtransaction', signed_raw_transaction)
+            raw_transaction = self.execute_rpc(
+                'createrawtransaction',
+                [{
+                    'txid': tx_chain.current_unspent_tx,
+                    'vout': 0,
+                }],
+                OrderedDict([
+                    (tx_chain.address, tx_chain.amount / 100000000),
+                    (self.spent_to.address, tx_chain.amount / 100000000)
+                ])
+            )
+            signed_raw_transaction = self.execute_rpc(
+                'signrawtransaction', raw_transaction
+            )['hex']
+            tx_chain.current_unspent_tx = self.execute_rpc(
+                'sendrawtransaction',
+                signed_raw_transaction
+            )
 
     def generate_tx(self):
         tx_chain = self.get_next_tx_chain()
         txid = lx(tx_chain.current_unspent_tx)
-        txins = [CMutableTxIn(COutPoint(txid, 0)), CMutableTxIn(COutPoint(txid, 1))]
+        txins = [
+            CMutableTxIn(COutPoint(txid, 0)),
+            CMutableTxIn(COutPoint(txid, 1))
+        ]
         txin_seckeys = [tx_chain.seckey, self.spent_to.seckey]
 
         amount_in = tx_chain.amount
         tx_chain.amount -= int(config.transaction_fee / 2)
 
-        txout1 = CMutableTxOut(tx_chain.amount, CBitcoinAddress(tx_chain.address).to_scriptPubKey())
-        txout2 = CMutableTxOut(tx_chain.amount, CBitcoinAddress(self.spent_to.address).to_scriptPubKey())
+        txout1 = CMutableTxOut(
+            tx_chain.amount,
+            CBitcoinAddress(tx_chain.address).to_scriptPubKey()
+        )
+        txout2 = CMutableTxOut(
+            tx_chain.amount,
+            CBitcoinAddress(self.spent_to.address).to_scriptPubKey()
+        )
 
         tx = CMutableTransaction(txins, [txout1, txout2], nVersion=2)
 
         for i, txin in enumerate(txins):
-            txin_scriptPubKey = CScript([OP_DUP, OP_HASH160, Hash160(txin_seckeys[i].pub), OP_EQUALVERIFY, OP_CHECKSIG])
+            txin_scriptPubKey = CScript([
+                OP_DUP,
+                OP_HASH160,
+                Hash160(txin_seckeys[i].pub),
+                OP_EQUALVERIFY,
+                OP_CHECKSIG
+            ])
             sighash = SignatureHash(txin_scriptPubKey, tx, i, SIGHASH_ALL)
             sig = txin_seckeys[i].sign(sighash) + bytes([SIGHASH_ALL])
             txin.scriptSig = CScript([sig, txin_seckeys[i].pub])
 
         tx_serialized = tx.serialize()
-        logging.info('{} trying to sendrawtransaction (in=2x{} out=2x{} fee={} bytes={}) using tx_chain number={}'
-                     .format(self.name, amount_in, txout1.nValue,
-                             (amount_in * 2) - (txout1.nValue * 2), len(tx_serialized),
-                             self.current_tx_chain_index))
+        logging.info(
+            '{} trying to sendrawtransaction'
+            ' (in=2x{} out=2x{} fee={} bytes={})'
+            ' using tx_chain number={}'
+            .format(self.name,
+                    amount_in,
+                    txout1.nValue,
+                    (amount_in * 2) - (txout1.nValue * 2),
+                    len(tx_serialized),
+                    self.current_tx_chain_index)
+        )
         tx_hash = self.execute_rpc('sendrawtransaction', b2x(tx_serialized))
         tx_chain.current_unspent_tx = tx_hash
-        logging.info('{} sendrawtransaction was successful; tx got hash={}'.format(self.name, tx_hash))
+        logging.info(
+            '{} sendrawtransaction was successful; tx got hash={}'
+                .format(self.name, tx_hash)
+        )
 
     def set_spent_to_address(self):
         address = self.execute_rpc('getnewaddress')
@@ -156,14 +212,22 @@ class BitcoinNode(Node):
 
     def create_tx_chains(self):
         for unspent_tx in self.execute_rpc('listunspent'):
-            seckey = CBitcoinSecret(self.execute_rpc('dumpprivkey', unspent_tx['address']))
-            tx_chain = TxChain(unspent_tx['txid'], unspent_tx['address'], seckey, unspent_tx['amount'] * 100000000)
+            seckey = CBitcoinSecret(
+                self.execute_rpc('dumpprivkey', unspent_tx['address'])
+            )
+            tx_chain = TxChain(
+                unspent_tx['txid'],
+                unspent_tx['address'],
+                seckey,
+                unspent_tx['amount'] * 100000000
+            )
 
             self.tx_chains.append(tx_chain)
 
     def get_next_tx_chain(self):
         tx_chain = self.tx_chains[self.current_tx_chain_index]
-        self.current_tx_chain_index = (self.current_tx_chain_index + 1) % len(self.tx_chains)
+        self.current_tx_chain_index = ((self.current_tx_chain_index + 1) %
+                                       len(self.tx_chains))
 
         return tx_chain
 
@@ -201,7 +265,9 @@ class ProxyNode(Node, PublicNode):
 
     def wait_for_highest_tip_of_node(self, node):
         block_hash = node.execute_rpc('getbestblockhash')
-        while block_hash != bash.check_output(proxycmd.get_best_public_block_hash(self.name)):
+        best_block_hash = bash.check_output(
+            proxycmd.get_best_public_block_hash(self.name))
+        while block_hash != best_block_hash:
             utils.sleep(0.2)
             logging.debug('Waiting for  blocks to spread...')
 
@@ -209,7 +275,9 @@ class ProxyNode(Node, PublicNode):
         raise NotImplemented('Attach proxy dir to host and then cat log.')
 
     def grep_log_for_errors(self):
-        raise NotImplemented('Attach proxy dir to host and then grep for errors.')
+        raise NotImplemented(
+            'Attach proxy dir to host and then grep for errors.'
+        )
 
     def add_latency(self, zones):
         for cmd in tccmd.create(self.name, zones, self.latency):
