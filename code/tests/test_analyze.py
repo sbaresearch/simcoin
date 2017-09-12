b0db3d559405789294bdeba4a07eb86e0fcd2816
Simon Mulser <simonmulser@gmail.com>
2017-09-12 13:32:33 +0200

    remove calculation of std/median for tips

--- code/tests/test_analyze.py
+++ code/tests/test_analyze.py
@@ -11,6 +11,7 @@ from parse import RPCExceptionLogLine
 from parse import ExceptionLogLine
 from analyze import Analyzer
 from mock import Mock
+from parse import ReceivedEvent
 
 
 class TestAnalyze(TestCase):
@@ -23,8 +24,6 @@ class TestAnalyze(TestCase):
             'block_hash': block_stats,
         }
 
-        blocks['block_hash'].receiving_timestamps = np.array([5, 7])
-
         context = Mock()
         context.parsed_blocks = blocks
         context.consensus_chain = ['block_hash']
@@ -34,10 +33,9 @@ class TestAnalyze(TestCase):
         m_open.assert_called_with(config.blocks_csv, 'w')
         handle = m_open()
         self.assertEqual(handle.write.call_count, 2)
-        self.assertEqual(handle.write.call_args_list[0][0][0], 'block_hash;node;timestamp;stale;height;total_size;'
-                                                               'txs;received_by;'
-                                                               'median_propagation;std_propagation\r\n')
-        self.assertEqual(handle.write.call_args_list[1][0][0], 'block_hash;node-0;1;Accepted;2;3;4;2;6.0;1.0\r\n')
+        self.assertEqual(handle.write.call_args_list[0][0][0], 'block_hash;node;timestamp;stale;height;'
+                                                               'total_size;txs\r\n')
+        self.assertEqual(handle.write.call_args_list[1][0][0], 'block_hash;node-0;1;Accepted;2;3;4\r\n')
 
     @patch('builtins.open', new_callable=mock_open)
     def test_create_block_csv_stale_block(self, m_open):
@@ -47,8 +45,6 @@ class TestAnalyze(TestCase):
             'block_hash': block_stats,
         }
 
-        blocks['block_hash'].receiving_timestamps = np.array([5, 7])
-
         context = Mock()
         context.parsed_blocks = blocks
         context.consensus_chain = []
@@ -57,15 +53,13 @@ class TestAnalyze(TestCase):
 
         m_open.assert_called_with(config.blocks_csv, 'w')
         handle = m_open()
-        self.assertEqual(handle.write.call_args_list[1][0][0], 'block_hash;node-0;1;Stale;2;3;4;2;6.0;1.0\r\n')
+        self.assertEqual(handle.write.call_args_list[1][0][0], 'block_hash;node-0;1;Stale;2;3;4\r\n')
 
     @patch('builtins.open', new_callable=mock_open)
     def test_create_tx_csv(self, m_open):
         txs = {
             'tx_hash': TxStats(1, 'node-0', 'tx_hash'),
         }
-
-        txs['tx_hash'].receiving_timestamps = np.array([5, 7])
 
         context = Mock()
         context.parsed_txs = txs
@@ -75,9 +69,8 @@ class TestAnalyze(TestCase):
         m_open.assert_called_with(config.txs_csv, 'w')
         handle = m_open()
         self.assertEqual(handle.write.call_count, 2)
-        self.assertEqual(handle.write.call_args_list[0][0][0], 'tx_hash;node;timestamp;'
-                                                               'accepted_by;median_propagation;std_propagation\r\n')
-        self.assertEqual(handle.write.call_args_list[1][0][0], 'tx_hash;node-0;1;2;6.0;1.0\r\n')
+        self.assertEqual(handle.write.call_args_list[0][0][0], 'tx_hash;node;timestamp\r\n')
+        self.assertEqual(handle.write.call_args_list[1][0][0], 'tx_hash;node-0;1\r\n')
 
     @patch('builtins.open', new_callable=mock_open)
     def test_create_tx_exceptions_csv(self, m_open):
