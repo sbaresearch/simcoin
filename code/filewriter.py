c96eb02846c0611727b74adbea11412bc35748f9
Simon Mulser <simonmulser@gmail.com>
2017-09-12 17:28:21 +0200

    make write_csv static again

--- code/filewriter.py
+++ code/filewriter.py
@@ -19,15 +19,15 @@ class FileWriter:
         self.args = utils.read_json_file(config.args_json)
 
     def execute(self):
-        self.write_csv(config.blocks_csv, BlockEvent.csv_header(), self.context.parsed_blocks.values())
-        self.write_csv(config.txs_csv, TxEvent.csv_header(), self.context.parsed_txs.values())
-        self.write_csv(config.tx_exceptions_csv, ExceptionEvent.csv_header(), self.context.tx_exceptions)
-        self.write_csv(config.block_exceptions_csv, ExceptionEvent.csv_header(), self.context.block_exceptions)
-        self.write_csv(config.tick_infos_csv, TickEvent.csv_header(), self.context.tick_infos)
-        self.write_csv(config.rpc_exceptions_csv, RPCExceptionEvent.csv_header(), self.context.rpc_exceptions)
-        self.write_csv(config.blocks_received_csv, ReceivedEvent.csv_header(), self.context.blocks_received)
-        self.write_csv(config.txs_received_csv, ReceivedEvent.csv_header(), self.context.txs_received)
-        self.write_csv(config.tips_csv, Tip.csv_header(), self.context.tips)
+        write_csv(config.blocks_csv, BlockEvent.csv_header(), self.context.parsed_blocks.values(), self.args.tag)
+        write_csv(config.txs_csv, TxEvent.csv_header(), self.context.parsed_txs.values(), self.args.tag)
+        write_csv(config.tx_exceptions_csv, ExceptionEvent.csv_header(), self.context.tx_exceptions, self.args.tag)
+        write_csv(config.block_exceptions_csv, ExceptionEvent.csv_header(), self.context.block_exceptions, self.args.tag)
+        write_csv(config.tick_infos_csv, TickEvent.csv_header(), self.context.tick_infos, self.args.tag)
+        write_csv(config.rpc_exceptions_csv, RPCExceptionEvent.csv_header(), self.context.rpc_exceptions, self.args.tag)
+        write_csv(config.blocks_received_csv, ReceivedEvent.csv_header(), self.context.blocks_received, self.args.tag)
+        write_csv(config.txs_received_csv, ReceivedEvent.csv_header(), self.context.txs_received, self.args.tag)
+        write_csv(config.tips_csv, Tip.csv_header(), self.context.tips, self.args.tag)
 
         self.create_general_infos_json()
 
@@ -38,13 +38,14 @@ class FileWriter:
         with open(config.general_infos_json, 'w') as file:
             file.write('{}\n'.format(json.dumps(self.context.general_infos)))
 
-    def write_csv(self, file, header, elements):
-        with open(file, 'w') as file:
-            w = csv.writer(file, delimiter=';')
-            header.append('tag')
-            w.writerow(header)
 
-            for element in elements:
-                row = element.vars_to_array()
-                row.append(self.args.tag)
-                w.writerow(row)
+def write_csv(file, header, elements, tag):
+    with open(file, 'w') as file:
+        w = csv.writer(file, delimiter=';')
+        header.append('tag')
+        w.writerow(header)
+
+        for element in elements:
+            row = element.vars_to_array()
+            row.append(tag)
+            w.writerow(row)
