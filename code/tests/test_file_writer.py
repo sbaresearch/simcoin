d7c84d8c03b41174f1febaa462d5a588107fc705
Simon Mulser <simonmulser@gmail.com>
2017-09-12 15:31:38 +0200

    add test for write_csv

--- code/tests/test_file_writer.py
+++ code/tests/test_file_writer.py
@@ -1,5 +1,19 @@
 from unittest import TestCase
+from mock import mock_open
+from mock import patch
+from mock import Mock
+import filewriter
 
 
 class TestFileWriter(TestCase):
-    pass
+
+    @patch('builtins.open', new_callable=mock_open)
+    def test_write_csv(self, m_open):
+        elements = Mock()
+        elements.vars_to_array.return_value = ['content_1', 'content_2']
+        filewriter.write_csv('file.name', ['header_1', 'header_2'], [elements])
+
+        m_open.assert_called_with('file.name', 'w')
+        handle = m_open()
+        self.assertEqual(handle.write.call_args_list[0][0][0], 'header_1;header_2\r\n')
+        self.assertEqual(handle.write.call_args_list[1][0][0], 'content_1;content_2\r\n')
