914b9fe90cd86275de2cab6526b6980a51548143
Simon Mulser <simonmulser@gmail.com>
2017-09-22 10:48:08 +0200

    remove old test

--- code/tests/test_postprocessing.py
+++ code/tests/test_postprocessing.py
@@ -14,40 +14,6 @@ class TestPostProcessing(TestCase):
         self.context = MagicMock()
         self.postprocessing = PostProcessing(self.context)
 
-    @patch('builtins.open', new_callable=mock_open)
-    @patch('bash.check_output_without_log', lambda cmd: '3\n4\n')
-    @patch('postprocessing.prefix_log', lambda lines, node: lines)
-    @patch('postprocessing.add_line_number', lambda lines: lines)
-    def test_aggregate_logs(self, m_open):
-        node_0 = MagicMock()
-        node_0.cat_log_cmd.return_value = "1\n2\n"
-        self.context.all_nodes = {'node-0': node_0}
-        self.context.path.aggregated_log = '/path'
-
-        self.postprocessing.aggregate_logs()
-
-        self.assertEqual(m_open.call_args[0][0], '/path')
-        handle = m_open()
-        self.assertEqual(handle.write.call_args_list[0][0][0], '1\n2\n')
-        self.assertEqual(handle.write.call_args_list[1][0][0], '3\n4\n')
-
-    @patch('builtins.open', new_callable=mock_open)
-    @patch('bash.check_output_without_log', lambda cmd: '')
-    @patch('postprocessing.prefix_log', lambda lines, node: lines)
-    @patch('postprocessing.add_line_number', lambda lines: lines)
-    def test_aggregate_logs_two_nodes(self, m_open):
-        node_0 = MagicMock()
-        node_1 = MagicMock()
-        node_0.cat_log_cmd.return_value = "1\n2\n"
-        node_1.cat_log_cmd.return_value = "11\n22\n"
-        self.context.all_nodes = OrderedDict(sorted({'node-0': node_0, 'node-1': node_1}.items(), key=lambda t: t[0]))
-
-        self.postprocessing.aggregate_logs()
-
-        handle = m_open()
-        self.assertEqual(handle.write.call_args_list[0][0][0], '1\n2\n')
-        self.assertEqual(handle.write.call_args_list[1][0][0], '11\n22\n')
-
     def test_prefix_log_no_changes(self):
         lines = ['2017-07-05 14:33:35.324000 test', '2017-07-05 14:33:35.324000 test']
         received = postprocessing.prefix_log(lines, 'node-0')
