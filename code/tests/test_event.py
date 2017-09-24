ef3ba16d50773d6ba8934c9533b2212e5fba1d97
Simon Mulser <simonmulser@gmail.com>
2017-09-24 12:15:05 +0200

    fix tests

--- code/tests/test_event.py
+++ code/tests/test_event.py
@@ -1,10 +1,9 @@
 from unittest import TestCase
 from event import Event
 from unittest.mock import patch
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 from unittest.mock import mock_open
 from bitcoinrpc.authproxy import JSONRPCException
-from textwrap import dedent
 
 
 class TestEvent(TestCase):
@@ -13,15 +12,17 @@ class TestEvent(TestCase):
     @patch('utils.sleep')
     @patch('utils.check_for_file', lambda file: None)
     def test_execute_multiple_cmds(self, m_sleep, m_time):
-        data = dedent("""
-            cmd1;cmd2;cmd3
-        """).strip()
+        m_file = mock_open(read_data=''.join(
+            'cmd1;cmd2;cmd3'
+        ))
+        m_file.return_value.__iter__ = lambda self: self
+        m_file.return_value.__next__ = lambda self: next(iter(self.readline, ''))
 
-        with patch('builtins.open', mock_open(read_data=data)):
-            mock = Mock()
+        with patch('builtins.open', m_file):
+            mock = MagicMock()
             mock.args.tick_duration = 1
             e = Event(mock)
-            e.execute_cmd = Mock()
+            e.execute_cmd = MagicMock()
 
             m_time.return_value = 0
 
@@ -34,16 +35,18 @@ class TestEvent(TestCase):
     @patch('utils.sleep')
     @patch('utils.check_for_file', lambda file: None)
     def test_execute_multiple_lines(self, m_sleep, m_time):
-        data = dedent("""
-            cmd1
-            cmd2
-        """).strip()
+        m_file = mock_open(read_data=''.join(
+            'cmd1\n'
+            'cmd2'
+        ))
+        m_file.return_value.__iter__ = lambda self: self
+        m_file.return_value.__next__ = lambda self: next(iter(self.readline, ''))
 
-        with patch('builtins.open', mock_open(read_data=data)):
-            mock = Mock()
+        with patch('builtins.open', m_file):
+            mock = MagicMock()
             mock.args.tick_duration = 1
             e = Event(mock)
-            e.execute_cmd = Mock()
+            e.execute_cmd = MagicMock()
 
             m_time.return_value = 0
 
@@ -56,15 +59,17 @@ class TestEvent(TestCase):
     @patch('utils.check_for_file', lambda file: None)
     @patch('logging.error')
     def test_execute(self, m_error, m_time):
-        data = dedent("""
-            cmd1;cmd2;cmd3
-        """).strip()
+        m_file = mock_open(read_data=''.join(
+            'cmd1;cmd2;cmd3'
+        ))
+        m_file.return_value.__iter__ = lambda self: self
+        m_file.return_value.__next__ = lambda self: next(iter(self.readline, ''))
 
-        with patch('builtins.open', mock_open(read_data=data)):
-            mock = Mock()
+        with patch('builtins.open', m_file):
+            mock = MagicMock()
             mock.args.tick_duration = 0
             e = Event(mock)
-            e.execute_cmd = Mock()
+            e.execute_cmd = MagicMock()
 
             m_time.side_effect = [0, 1, 10]
 
@@ -74,45 +79,47 @@ class TestEvent(TestCase):
     @patch('utils.check_for_file', lambda file: None)
     @patch('logging.error')
     def test_execute_with_exce_execute_cmd(self, m_error):
-        data = dedent("""
-            cmd1
-        """).strip()
+        m_file = mock_open(read_data=''.join(
+            'cmd1'
+        ))
+        m_file.return_value.__iter__ = lambda self: self
+        m_file.return_value.__next__ = lambda self: next(iter(self.readline, ''))
 
-        with patch('builtins.open', mock_open(read_data=data)):
-            mock = Mock()
+        with patch('builtins.open', m_file):
+            mock = MagicMock()
             mock.args.tick_duration = 0
             e = Event(mock)
-            e.execute_cmd = Mock()
+            e.execute_cmd = MagicMock()
             e.execute_cmd.side_effect = Exception('mock')
 
             e.execute()
             self.assertRegex(m_error.call_args[0][0], 'mock')
 
     def test_execute_cmd_with_block_cmd(self):
-        node_1 = Mock()
+        node_1 = MagicMock()
         cmd = 'block node-1'
-        e = Event(Mock())
+        e = Event(MagicMock())
         e.context.all_bitcoin_nodes = {'node-1': node_1}
         e.execute_cmd(cmd)
 
         self.assertTrue(node_1.generate_block.called)
 
     def test_execute_cmd_with_block_cmd_with_empty_cmd(self):
-        node_1 = Mock()
+        node_1 = MagicMock()
 
-        e = Event(Mock())
-        e.generate_tx = Mock()
+        e = Event(MagicMock())
+        e.generate_tx = MagicMock()
         e.execute_cmd('')
 
         self.assertFalse(node_1.execute_rpc.called)
         self.assertFalse(e.generate_tx.called)
 
     def test_execute_cmd_with_tx_tmd(self):
-        node = Mock()
+        node = MagicMock()
         cmd = 'tx node-1'
 
-        e = Event(Mock())
-        e.generate_tx = Mock()
+        e = Event(MagicMock())
+        e.generate_tx = MagicMock()
         e.context.all_bitcoin_nodes = {'node-1': node}
         e.execute_cmd(cmd)
 
@@ -120,7 +127,7 @@ class TestEvent(TestCase):
 
     def test_execute_cmd_with_unknown_cmd(self):
         cmd = 'unknown node-1'
-        e = Event(Mock())
+        e = Event(MagicMock())
         e.context.all_bitcoin_nodes = {'node-1': {}}
 
         with self.assertRaises(Exception) as context:
@@ -129,8 +136,8 @@ class TestEvent(TestCase):
         self.assertTrue('Unknown cmd' in str(context.exception))
 
     def test_execute_cmd_with_exception(self):
-        context = Mock()
-        node = Mock()
+        context = MagicMock()
+        node = MagicMock()
         node.generate_tx.side_effect = JSONRPCException({'code': -1, 'message': 'test_message'})
         context.all_bitcoin_nodes = {'node-1': node}
 
