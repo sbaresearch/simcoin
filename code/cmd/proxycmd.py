29610e6dfd253cd5f40f2e5841861b9acf8e376f
Simon Mulser <simonmulser@gmail.com>
2017-10-07 13:34:45 +0200

    make log of proxy verbose

--- code/cmd/proxycmd.py
+++ code/cmd/proxycmd.py
@@ -3,7 +3,8 @@ from cmd import dockercmd
 
 def run_proxy(node, start_hash, path):
     args = '{} '.format(node.args) if node.args else ''
-    return dockercmd.run_selfish_proxy(node, 'python main.py {} --ip-private {} --ips-public {} --start-hash {}'
+    return dockercmd.run_selfish_proxy(node, 'python main.py {} --verbose'
+                                             ' --ip-private {} --ips-public {} --start-hash {}'
                                        .format(args, str(node.private_ip), ' '.join(node.outgoing_ips), start_hash),
                                        path)
 
