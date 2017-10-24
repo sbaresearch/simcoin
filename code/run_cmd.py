5cca0a772570214eddd7ab5d7a8003c55af8dd92
Simon Mulser <simonmulser@gmail.com>
2017-10-24 10:42:22 +0200

    remove unused imports

--- code/run_cmd.py
+++ code/run_cmd.py
@@ -2,11 +2,6 @@ from simulationfiles import nodes_config
 from simulationfiles import ticks_config
 from simulationfiles import network_config
 import simulation_cmd
-import argparse
-import utils
-import logging
-import sys
-from simulationfiles import checkargs
 
 
 def run():
