ce1986e99fd170b8c0de90fc04424b5caceef1ec
Simon Mulser <simonmulser@gmail.com>
2017-11-20 15:44:40 +0100

    remove empty file

--- code/tests/test_executor.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-
-class TestExecutor(TestCase):
-
-    def __init__(self, *args, **kwargs):
-        super(TestExecutor, self).__init__(*args, **kwargs)
-        pass
