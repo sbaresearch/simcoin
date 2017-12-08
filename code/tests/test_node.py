8c2e3747c898e7dc4742a33c0895dc9ea178a178
Simon Mulser <simonmulser@gmail.com>
2017-12-08 14:26:03 +0100

    fix tests

--- code/tests/test_node.py
+++ code/tests/test_node.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 from node import BitcoinNode
 from mock import patch
+from mock import MagicMock
 from bitcoin.wallet import CBitcoinSecret
 import bitcoin
+import node as node_utils
 
 bitcoin.SelectParams('regtest')
 
@@ -38,3 +40,19 @@ class TestNode(TestCase):
         self.assertEqual(chain_2.current_unspent_tx, 'tx_hash_2')
         self.assertEqual(chain_2.address, 'address_hash_2')
         self.assertEqual(chain_2.amount, 2500000000)
+
+    @patch('utils.sleep')
+    def test_wait_until_height_reached(self, m_sleep):
+        node = MagicMock()
+        node.execute_rpc.side_effect = ['0', '9', '10']
+        node_utils.wait_until_height_reached(node, 10)
+
+        self.assertEqual(m_sleep.call_count, 2)
+
+    @patch('utils.sleep')
+    def test_wait_until_height_reached_already_reached(self, m_sleep):
+        node = MagicMock()
+        node.execute_rpc.return_value = '10'
+        node_utils.wait_until_height_reached(node, 10)
+
+        self.assertFalse(m_sleep.called)
--- code/tests/test_prepare.py
+++ code/tests/test_prepare.py
@@ -17,7 +17,7 @@ class TestPrepare(TestCase):
 
         bitcoin.SelectParams('regtest')
 
-    @patch('prepare._wait_until_height_reached', lambda node, height: None)
+    @patch('node.wait_until_height_reached', lambda node, height: None)
     @patch('utils.sleep', lambda time: None)
     @patch('prepare._calc_number_of_tx_chains', lambda txs_per_tick, block_per_tick, amount_of_nodes: 5)
     def test_warmup_block_generation(self):
@@ -38,10 +38,11 @@ class TestPrepare(TestCase):
     @patch('builtins.open', new_callable=mock_open)
     def test_prepare_simulation_dir(self, m_open, m_check_output, m_makedirs, m_exists):
         m_exists.return_value = False
+        self.prepare._pool = MagicMock()
 
         self.prepare._prepare_simulation_dir()
 
-        self.assertEqual(m_makedirs.call_count, 2)
+        self.assertEqual(m_makedirs.call_count, 3)
         self.assertEqual(m_check_output.call_count, 9)
 
     @patch('bash.check_output')
@@ -80,22 +81,6 @@ class TestPrepare(TestCase):
         prepare._recreate_network()
 
         self.assertEqual(m_check_output.call_count, 1)
-
-    @patch('utils.sleep')
-    def test_wait_until_height_reached(self, m_sleep):
-        node = MagicMock()
-        node.execute_rpc.side_effect = ['0', '9', '10']
-        prepare._wait_until_height_reached(node, 10)
-
-        self.assertEqual(m_sleep.call_count, 2)
-
-    @patch('utils.sleep')
-    def test_wait_until_height_reached_already_reached(self, m_sleep):
-        node = MagicMock()
-        node.execute_rpc.return_value = '10'
-        prepare._wait_until_height_reached(node, 10)
-
-        self.assertFalse(m_sleep.called)
 
     def test_calc_number_of_tx_chains(self):
         config.max_in_mempool_ancestors = 25
